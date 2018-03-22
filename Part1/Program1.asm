beea3937b0d51a868e4446f6a741a2aca75ee247
David <David.wisniewski.wor@gmail.com>
2018-03-21 20:16:22 -0400

    What are you doing array stop that

--- Part1/Program1.asm
+++ Part1/Program1.asm
@@ -3,6 +3,9 @@ strA:		.asciiz "Original Array:\n"
 strB:		.asciiz "Second Array:\n"
 newline:	.asciiz "\n"
 space:	 	.asciiz " "
+#This is just some whitespace for the output
+comma_string: 	.asciiz " , "
+tabstring:  	.asciiz "\t"
 
 #This is the start of the original array.
 Original:	.word 200, 270, 250, 100
@@ -29,7 +32,6 @@ la $a2, Second		#Loads the adress of the second array
 
 li $s1, 4		#The size of the x by x matrix
 li $s0, 4		#Constant to multiply by a word is 4 bytes
-li $k0, 4		#Where I want both counters to stop when i and j < 4
 mul $k1, $s0, $s1	# 4*4 =16
 
 #Test Output
@@ -37,85 +39,154 @@ mul $k1, $s0, $s1	# 4*4 =16
 #li $v0, 1	       	#$system call code for print integer
 #syscall		#Print the integer
 
-li $t1, 0		#Sets i to zero 
-li $t2, 0		#Sets j to zero
 
 ###################################################################################
-Loop001:
-beq $t1, $k0, EXIT	#If i == 3 EXIT
-li $t2, 0		#Sets j to zero
-#Test Output for i
-add $a0, $t1, $zero	#Transfers data from one register to another
-li $v0, 1	       	#$system call code for print integer
-syscall			#Print the integer
+li $t1, 0 	        # This is the start index for i
 
-#Infinate loop ??
-# Output 000000000000000000000000000000000000000000000000000000000000000000000000000000000 and crash
+Loop001:
+	li $t2, 0           # This is the start index for j
 
 Loop002:
-bltu $t2, $k0, Loop001	#If j== 3 Loop001
+# Print the values for 'i' and 'j' ($t0 and $t1)
+#First print 'i' ($t0)
+#	li $v0, 1          		 # system code 1 for printing an integer	          
+#    	add $a0, $zero, $t1		 # copy register $t1 to $a0
+     					    		
+#	syscall
 
-mul $t3, $t1, $k1	#(i*16)   
-mul $t4, $t2, $k1	#(j*16)
-mul $t5, $t1, $s0	#i*4
-mul $t6, $t2, $s0	#j*4
+#Print Newline
+# 	la $a0, newline     		#load address of whitespace into $a0
+#  	li $v0, 4           		#system code 4 for printing a string
+#	syscall
 
-add $t3, $t3, $t6	#(i*16)+j*4 The calculated offset
-add $t4, $t4, $t5	#(j*16)+i*4 The calculated offset
+#Now print 'j' ($t1)
+#	li $v0, 1           		# system code 1 for printing an integer	          
+#     	add $a0, $zero, $t1 		# copy register $t1 to $a0
+#	syscall
 
-add $t3, $a1, $t3	#Base plus offset for [i][j]
-add $t4, $a1, $t4	#Base plus offset for [j][i]
 
-lw $v0, ($t3)		#Get Original[i][j]
-lw $v1, ($t4)		#Get Original[j][i]
-lw $t5, ($t3)		#Get Second[i][j]
-lw $t6, ($t4)		#Get Second[j][i]
+	mul $t3, $t1, $k1		#(i*16)
+	mul $t6, $t2, $s0		#j*4
+	   
+	mul $t4, $t2, $k1		#(j*16)
+	mul $t5, $t1, $s0		#i*4
+	
 
-add $t5, $v1, $zero	#Moves the data from v1 to t5
-add $t6, $v0, $zero	#Moves the data from v0 to t6
+	add $t3, $t3, $t6		#(i*16)+j*4 The calculated offset
+	add $t4, $t4, $t5		#(j*16)+i*4 The calculated offset
 
-		
+	add $t3, $a1, $t3		#Base plus offset for [i][j]
+	add $t4, $a1, $t4		#Base plus offset for [j][i]
 
-addi $t2, $t2, 1	# Increments J+1
+	lw $t5, ($t3)			#Get Original[i][j]
+	lw $t6, ($t4)			#Get Second[j][i]
+	
+	li $v0, 1           		# system code 1 for printing an integer	          
+     	add $a0, $zero, $t5 		# copy register $t5 to $a0
+     	syscall 
+     	
+ 	la $a0, space 			# load address of comma_stringinto $a0
+  	li $v0, 4            		# system code 4 for printing a string
+	syscall
 
-j Loop002		#Jumps to Label Loop002
+	add $t6, $zero, $t5		#Moves the data from t5 to t6
+	sw $t6, ($t4)			#Stores the data in the second array
+	
+	
+#This is the inner loop branch part
+	addi $t2, $t2, 1    		# j <- j + 1 (increment count)
 
-addi $t1, $t1, 1	# Increments I+1
+     	slti  $t5, $t2, 4 		# 'set less than (immediate) instr. 
+				    	# if( $t2 < 4 )
+				   	#   $t5 <- 1
+				    	# else
+				    	#   $t5 <- 0     	
 
-j Loop001		#Jumps to Label Loop001
+#branch if $t5 != 0 (which means $t1 < 4 because of the slti)
+	bne $t5, $zero, Loop002
 
-EXIT:
-li $t1, 0		#Sets i to zero 
-li $t2, 0		#Sets j to zero
+
+ 	la $a0, newline     		#load address of whitespace into $a0
+  	li $v0, 4           		#system code 4 for printing a string
+	syscall
+
+#This is the outer loop branch part
+
+	addi $t1, $t1, 1   		# i <- i+1 (increment count)
+
+     	slti  $t5, $t1, 4 		# 'set less than (immediate) instr. 
+				   	# if( $t0 < 10 )
+				   	#   $t5 <- 1
+				    	# else
+				    	#   $t5 <- 0     	
+
+#branch if $t5 != 0 (which means $t0 < 4 because of the slti)
+	bne $t5, $zero, Loop001
+
+##########################################################################################
+li $t1, 0 	        		# This is the start index for i
 
 Loop003:
-beq $t1, $k0, END	#If i == 3 END
-li $t2, 0		#Sets j to zero
-
+	li $t2, 0           		# This is the start index for j
 
 Loop004:
-beq $t2, $k0, Loop003	#If j== 3 EXIT
-mul $t3, $t1, $k1	#(i*16)
-mul $t5, $t2, $s0	#j*4
-add $t3, $t3, $t5	#(i*16)+j*4 The calculated offset
-add $t3, $a1, $t3	# Base plus offset for [i][j]
-lw $t5, ($t3)		#Get Second[i][j]
-add $a0, $t5, $zero	#Puts the value of Second[i][j] in $a0
-li $v0, 1	       	#$system call code for print integer
-syscall			#Print the integer
-# print space, 32 is ASCII code for space
-li $a0, 32
-li $v0, 11  # syscall number for printing character
-syscall
+# Print the values for 'i' and 'j' ($t0 and $t1)
+#First print 'i' ($t0)
+	li $v0, 1          		 # system code 1 for printing an integer	          
+     	add $a0, $zero, $t1		 # copy register $t1 to $a0
+     					    		
+	syscall
 
-addi $t2, $t2, 1	# Increments J+1
+	mul $t3, $t1, $k1		#(i*16)   
+	mul $t6, $t2, $s0		#j*4
 
-j Loop003		#Jumps to Label Loop003
+	add $t3, $t3, $t6		#(i*16)+j*4 The calculated offset
+	add $t3, $a1, $t3		#Base plus offset for [i][j]
 
-addi $t1, $t1, 1	# Increments I+1
+	lw $t6, ($t4)			#Get Second[j][i]
 
-j Loop004		#Jumps to Label Loop004
+#Now print 'j' ($t1)
+	li $v0, 1           		# system code 1 for printing an integer	          
+     	add $a0, $zero, $t6 		# copy register $t1 to $a0
+     	syscall 
+			
+#Now print the space "  "
+ 	la $a0, space 			# load address of comma_stringinto $a0
+  	li $v0, 4            		# system code 4 for printing a string
+	syscall
 
-END:
-syscall 
+	
+#This is the inner loop branch part
+	addi $t2, $t2, 1    		# j <- j + 1 (increment count)
 
+     	slti  $t5, $t2, 4 		# 'set less than (immediate) instr. 
+				    	# if( $t2 < 4 )
+				   	#   $t5 <- 1
+				    	# else
+				    	#   $t5 <- 0     	
+
+#branch if $t5 != 0 (which means $t1 < 4 because of the slti)
+	bne $t5, $zero, Loop004
+
+#Now print newline "\n"
+ 	la $a0, newline     		#load address of whitespace into $a0
+  	li $v0, 4           		#system code 4 for printing a string
+	syscall
+
+#This is the outer loop branch part
+
+	addi $t1, $t1, 1   		# i <- i+1 (increment count)
+
+     	slti  $t5, $t1, 4 		# 'set less than (immediate) instr. 
+				   	# if( $t0 < 10 )
+				   	#   $t5 <- 1
+				    	# else
+				    	#   $t5 <- 0     	
+
+#branch if $t5 != 0 (which means $t0 < 4 because of the slti)
+	bne $t5, $zero, Loop003
+
+
+#Exit system call
+li $v0, 10 # Sets $v0 to "10" to select exit syscall
+syscall # Exit
